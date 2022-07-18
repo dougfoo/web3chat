a5359e42eed9a44733b3fba53cf3d198bbc09073
Doug Foo <doug.cha@gmail.com>
2022-07-18 12:04:45 +0900

    fix key

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 cd build
-call az storage copy --account-key SWhDOW6+rAmzrVBzMA4D7qFjA4Ry4Ld956sbupvF6USpvdkp7xeWaVMTZ22gVTiFvfWamxSVAe2F+AStgm+dHg== -s *.* -d https://web3static.blob.core.windows.net/$web --recursive
+call az storage copy --account-key $key -s *.* -d https://web3static.blob.core.windows.net/$web --recursive
 echo "done 1"
-call az storage copy --account-key SWhDOW6+rAmzrVBzMA4D7qFjA4Ry4Ld956sbupvF6USpvdkp7xeWaVMTZ22gVTiFvfWamxSVAe2F+AStgm+dHg== -s static -d https://web3static.blob.core.windows.net/$web --recursive 
+call az storage copy --account-key $key -s static -d https://web3static.blob.core.windows.net/$web --recursive 
 echo "done 2"

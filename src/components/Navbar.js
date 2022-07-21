97e5249d1253fe23070b975a90fe5ad23e10068c
Doug Foo <doug.cha@gmail.com>
2022-07-21 18:39:29 +0900

    adding about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Badge from 'react-bootstrap/Badge';
 
 class Navbar extends Component {
 
@@ -11,7 +12,15 @@ class Navbar extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          FooStack web3's Blockchain Demo
+          FooStack web3's Blockchain Demo 
+        </a>
+        <a
+          className="navbar-light col-sm-3 col-md-2 mr-0"
+          href="/tech"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About the Tech 
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">

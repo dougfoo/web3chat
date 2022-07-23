e23bd87e1095ea8ed4e3060c56a10d17e09cc710
dougfoo <doug.cha@gmail.com>
2022-07-23 20:47:27 +0900

    updated condensed tech stack info

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,34 +19,31 @@ function MyVerticallyCenteredModal(props) {
       <Modal.Body>
         <h4>Browser and ReactJS and Web3 API</h4>
         <p>
-        At the top layer is a ReactJS compatible browser using bootstrap UI components.  I love React.
-        Web3 is a JS API that greatly simplifies interaction with Etherium and Smart Contracts and enables it for JS development.
+        <li>ReactJS compatible browser using bootstrap UI components.  I love React.</li>
+        <li>Web3 is a JS API interface for Etherium and Smart Contracts.</li>
         </p>
         <hr></hr>
         <h4>Etherium and the MetaMask Wallet</h4>
         <p>
-        Etherium (ETH) is the blockchain (2nd to BTC in popularity) - and it's claim to fame is Smart Contracts.  
-        Contracts enable storage of arbitrary "things" on the blockchain.   
-        To use ETH, a Wallet is needed and some ETH (cash - since nothing is free).  
-        MetaMask is a Chrome based Wallet -- <i>I wouldn't recommend storing your $millions here, but it is ok for goofing around.</i>   
-        Also note NFT's are the most famous Smart Contracts but most others are prototypes and deployed on test networks like Ropsten, 
-        just like my demo.
+        <li>Etherium (ETH) is the blockchain (2nd to BTC in popularity) - and it's claim to fame is Smart Contracts 
+          enable storage of arbitrary data on the blockchain.</li>   
+        <li>To use ETH, a Wallet is needed - MetaMask is a popular Chrome based Wallet -- <i>Don't store your life savings here.</i> </li>  
+        <i>Also note NFT's are the most famous (only) Smart Contracts in production.</i>
         </p>
         <hr></hr>
         <h4>Smart Contracts and Solidity</h4>
         <p>
-        A Smart Contract is really just an IDL / Interface file written in Solidity that binds arbitrary data to the blockchain.
-        A compiled contract is registered to an ETH blockchain and then anyone can use it.  
-        I defined a simple contract to store chat messages and likes, and registered it to the Ropsten Test ETH blockchain.   
-        Note it costs ETH tokens (gas) to register contracts as well as to use them to write a chat message or like one!  (On Repsten
-        it is all test and fake $$$ thank god).
+        <li>A Smart Contract is like an IDL / Interface for binding arbitrary data to the blockchain.  
+          Contracts are registered to an ETH blockchain then usable by all.</li>  
+        <li>I defined a simple contract to store chat messages and likes, and registered it to the Ropsten Test ETH network.   
+        <i>Note it costs ETH tokens (gas) to register contracts and write data to the blockchain.  On Roptsten it is free</i>.</li>
         </p>
         <hr></hr>
         <h4>Backend - what backend?</h4>
         <p>
-        Note there is no backend.  This app you are using is a static HTML/JS webpack hosted in an Azure blob.  
-        It reads/stores chat data from the ETH blockchain contracts, using Web3 and MetaMask wallets 
-        and renders client side using ReactJS and Web3 API.
+        <li>Note there is no backend.  This app is a static HTML/JS webpack hosted in an Azure blob.</li>  
+        <li>It reads/stores chat data from the ETH blockchain using Web3 and MetaMask wallets 
+        and renders client side using ReactJS.</li>
         Think of this as a <b>really slow, unscalable, and expensive distributed database that has no privacy</b>!
         </p>
       </Modal.Body>
